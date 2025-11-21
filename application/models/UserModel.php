bb7b2268aba02d73fb0a62e4bf3528dd9fa1d432
egor.linux <egor.bazakin.09@gmail.com>
2025-11-21 11:52:33 +0000

    Enhanced insert and update functions: added password hashing; updating can executed without pass value

--- application/models/UserModel.php
+++ application/models/UserModel.php
@@ -67,20 +67,30 @@ class UserModel extends Model
     
     public function update()
     {
-        $sql = "UPDATE $this->tableName SET timestamp=:timestamp, login=:login, pass=:pass, email=:email  WHERE id = :id";  
+        $sql = "UPDATE $this->tableName SET timestamp=:timestamp, login=:login, email=:email, role=:role";  
+        $clause =  " WHERE id = :id";
+
+        if (!empty($this->pass)) {
+            $sql .= ", pass=:pass, salt=:salt";
+        }
+
+        $sql .= $clause;
+
         $st = $this->pdo->prepare ( $sql );
         
         $st->bindValue( ":timestamp", (new \DateTime('NOW'))->format('Y-m-d H:i:s'), \PDO::PARAM_STMT);
         $st->bindValue( ":login", $this->login, \PDO::PARAM_STR );
         
         // Хеширование пароля
-        $this->salt = rand(0,1000000);
-        //$st->bindValue( ":salt", $this->salt, \PDO::PARAM_STR );
-        //$this->pass .= $this->salt;
-        //$hashPass = password_hash($this->pass, PASSWORD_BCRYPT);
-        $st->bindValue( ":pass", $this->pass, \PDO::PARAM_STR );
+        if (!empty($this->pass)) {
+            $this->salt = rand(0,1000000);
+            $st->bindValue( ":salt", $this->salt, \PDO::PARAM_STR );
+            $this->pass .= $this->salt;
+            $hashPass = password_hash($this->pass, PASSWORD_BCRYPT);
+            $st->bindValue( ":pass", $hashPass, \PDO::PARAM_STR );
+        }
         
-        //$st->bindValue( ":role", $this->role, \PDO::PARAM_STR );
+        $st->bindValue( ":role", $this->role, \PDO::PARAM_STR );
         $st->bindValue( ":email", $this->email, \PDO::PARAM_STR );
         $st->bindValue( ":id", $this->id, \PDO::PARAM_INT );
         $st->execute();
